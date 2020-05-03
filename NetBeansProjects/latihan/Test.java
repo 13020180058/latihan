08ac6b427091300a3edc21c600585ddfa68ffc44
Apriani magfira <57305690+13020180058@users.noreply.github.com>
2020-05-03 21:56:43 +0800

    Add files via upload

---
+++ NetBeansProjects/latihan/Test.java
@@ -0,0 +1,19 @@
+
+package latihan;
+
+/*NAMA ; APRIANI MAGFIRA
+STAMBUK : 13020180058
+TANGGAL/WAKTU ;01 MEI 2020 / 21.00 
+ */
+
+public class Test 
+  { public static int counter=0;
+    public Test()
+    { counter +=1; }
+    
+     public int getCounter()
+     {  return counter;  }     
+     
+     public void addCounter()
+     {  counter +=1;  }     
+  }
